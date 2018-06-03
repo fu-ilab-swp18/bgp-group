bf05c192a110ea4c1c094f695703430718605864
Max Stauss <max.stauss@gmail.com>
2018-06-03 15:25:17 +0200

    init create tables

---
+++ BGPDisplay/classes/sql/sqlite/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE vantagepoint_metadata (
+   vpid TEXT NOT NULL,
+   rcid TEXT NOT NULL,
+   timestamp INTEGER NOT NULL,
+   valid INTEGER,
+   invalid INTEGER,
+   unknown INTEGER,
+   PRIMARY KEY (vpid, rcid, timestamp)
+);
+
+CREATE TABLE routecollector_metadata (
+   rcid TEXT NOT NULL,
+   timestamp INTEGER NOT NULL,
+   rcloc TEXT NOT NULL,
+   peers INTEGER,
+   prefix4 INTEGER,
+   prefix6 INTEGER,
+   unknown INTEGER,
+   PRIMARY KEY (rcid, timestamp)
+);
