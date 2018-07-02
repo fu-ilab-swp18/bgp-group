1971c99fd9679873d1b1e75f45ffe494b17d1e44
Max Stauss <max.stauss@gmail.com>
2018-07-02 15:02:49 +0200

    update sql to update anyway

--- BGPDisplay/classes/sql/postgres/update_rc_meta.sql
+++ BGPDisplay/classes/sql/postgres/update_rc_meta.sql
@@ -1,1 +1,8 @@
-INSERT INTO "public"."RouteCollector_Metadata"("rcid", "timestamp", "peers", "prefix4", "prefix6") VALUES(%s, %s, %s, %s, %s);
+INSERT INTO "public"."RouteCollector_Metadata"("rcid", "timestamp", "peers", "prefix4", "prefix6")
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (rcid, "timestamp")
+DO UPDATE SET
+    peers = EXCLUDED.peers,
+    prefix4 = EXCLUDED.prefix4,
+    prefix6 = EXCLUDED.prefix6
+;
--- BGPDisplay/classes/sql/postgres/update_vp_meta.sql
+++ BGPDisplay/classes/sql/postgres/update_vp_meta.sql
@@ -2,7 +2,7 @@ INSERT INTO "public"."VantagePoint_Metadata"("vpid", "rcid", "timestamp", "valid
 VALUES(%s, %s, %s, %s, %s, %s)
 ON CONFLICT (vpid, rcid, "timestamp")
 DO UPDATE SET
-    vaild = EXCLUDED.valid,
+    valid = EXCLUDED.valid,
     unknown = EXCLUDED.unknown,
     invalid = EXCLUDED.invalid
 ;
