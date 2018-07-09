ec308f4d7ef04fad2f2fe583d0d6cdb3458d0596
Max Stauss <max.stauss@gmail.com>
2018-07-09 15:51:05 +0200

    fix vpaddr keyword

--- BGPDisplay/classes/sql/postgres/update_vp_meta.sql
+++ BGPDisplay/classes/sql/postgres/update_vp_meta.sql
@@ -1,6 +1,6 @@
 INSERT INTO "public"."VantagePoint_Data"("vpid", "vpaddr", "rcid", "timestamp", "valid", "unknown", "invalid")
 VALUES(%s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (vpid, prefix, rcid, "timestamp")
+ON CONFLICT (vpid, vpaddr, rcid, "timestamp")
 DO UPDATE SET
     valid = EXCLUDED.valid,
     unknown = EXCLUDED.unknown,
