e535371fbf83c244cca64c359b888884107d6438
Albrecht Oster <a.oster@online.de>
2018-07-11 15:35:19 +0200

    make information box rotate through slides translate times format numbers adjust titles

--- BGPDisplay/interface_js/index.js
+++ BGPDisplay/interface_js/index.js
@@ -1,16 +1,27 @@
 var blessed = require('blessed'),
     contrib = require('blessed-contrib'),
-    moment = require('moment');
+    moment = require('moment'),
+    numeral = require('numeral');
 
 var screen = blessed.screen();
 var grid = new contrib.grid({rows: 2, cols: 3, screen: screen});
 
 var apiData = require('./data');
 
+moment.locale('de');
+
+numeral.register('locale', 'de', {
+  delimiters: {
+    thousands: '.',
+    decimal: ','
+  }
+});
+numeral.locale('de');
+
 // announced prefixes
 
 var prefixesChart = grid.set(0, 0, 1, 2, contrib.line, {
-  label: 'Annoncierte Präfixe',
+  label: 'Neu annoncierte Präfixe',
   style: {
     line: 'yellow',
     text: 'white',
@@ -25,7 +36,7 @@ var prefixesChart = grid.set(0, 0, 1, 2, contrib.line, {
 // validated prefixes
 
 var validatedPrefixesChart = grid.set(1, 0, 1, 2, contrib.stackedBar, {
-  label: 'RPKI-validierte Präfixe',
+  label: 'RPKI-validierte Präfixe (in Prozent)',
   barWidth: 4,
   barSpacing: 15,
   xOffset: 0,
@@ -38,7 +49,7 @@ var validatedPrefixesChart = grid.set(1, 0, 1, 2, contrib.stackedBar, {
 
 var informationBox = grid.set(0, 2, 1, 1, blessed.box, {
   label: 'Allgemeines',
-  content: 'Was ist BGP?\nDas Border Gateway Protocol (BGP) ist das im Internet eingesetzte Routingprotokoll und verbindet autonome Systeme (AS) miteinander. Diese autonomen Systeme werden in der Regel von Internetdienstanbietern gebildet. BGP wird allgemein als Exterior-Gateway-Protokoll (EGP) und Pfadvektorprotokoll bezeichnet und verwendet für Routing-Entscheidungen sowohl strategische wie auch technisch-metrische Kriterien, wobei in der Praxis meist betriebswirtschaftliche Aspekte berücksichtigt werden.'
+  padding: 1
 });
 
 // statistics
@@ -104,10 +115,10 @@ function updateData() {
       data: [
         ['ID des Route Collectors:', selectedRC],
         ['Standort des RC:', 'DE-CIX Frankfurt'],
-        ['Anzahl Peers:', stats.peers],
-        ['Bekannte Präfixe:', stats.prefix],
-        ['Davon IPv6:', stats.prefix6],
-        ['Davon IPv4:', stats.prefix4],
+        ['Anzahl Peers:', numeral(stats.peers).format()],
+        ['Bekannte Präfixe:', numeral(stats.prefix).format()],
+        ['Davon IPv6:', numeral(stats.prefix6).format()],
+        ['Davon IPv4:', numeral(stats.prefix4).format()],
         ['Letztes Update:', moment.unix(stats.timestamp).format("DD. MMM YYYY, HH:mm")]
       ]
     });
@@ -118,4 +129,20 @@ function updateData() {
   });
 }
 
+var currentInformationSlide = 0;
+const informationSlides = [
+  'Was ist BGP?\nDas Border Gateway Protocol (BGP) ist das im Internet eingesetzte Routingprotokoll und verbindet autonome Systeme (AS) miteinander. Diese autonomen Systeme werden in der Regel von Internetdienstanbietern gebildet. BGP wird allgemein als Exterior-Gateway-Protokoll (EGP) und Pfadvektorprotokoll bezeichnet und verwendet für Routing-Entscheidungen sowohl strategische wie auch technisch-metrische Kriterien, wobei in der Praxis meist betriebswirtschaftliche Aspekte berücksichtigt werden.',
+  'Was ist RPKI?\nResource Public Key Infrastructure (RPKI), also known as Resource Certification, is a specialized public key infrastructure (PKI) framework designed to secure the Internet\'s routing infrastructure.',
+  'Was ist ein AS?\nEin autonomes System (AS) ist laut klassischer Definition eine Menge von Routern (die mehrere Netzwerke verbinden) mit einem gemeinsamen inneren Gateway-Protokoll (IGP) und gemeinsamen Metriken, die bestimmen, wie Pakete innerhalb eines AS vermittelt werden, unter einer einzigen technischen Verwaltung.'
+];
+
+function updateInformationBox() {
+  informationBox.setContent(informationSlides[currentInformationSlide]);
+  currentInformationSlide = ++currentInformationSlide % informationSlides.length;
+  screen.render();
+
+  setTimeout(updateInformationBox, 10000);
+}
+
 updateData();
+updateInformationBox();
